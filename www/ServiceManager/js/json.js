c00531fed9d009336e13e95031770997336d74ec
Julian Garcia-Sotoca <juliansotoca@gmail.com>
2013-10-24 16:28:31 +0000

    added ddbb and change host

--- www/ServiceManager/js/json.js
+++ www/ServiceManager/js/json.js
@@ -1,18 +1,28 @@
 var services={"services":[
         {
 			"service": "service1",
-            "backend":"server4",
+            "backend":"serverX",
+            "backup":"serverY",
             "frontedns": [
 				"fend1",
 				"fend2",
 				"fend3"
 			],
+			"bbdd": [
+				"bbdd1",
+				"bbdd2"
+			],
 			"other": "N/A"
         },
         {
 			"service": "service2",
-            "backend":"server3",
+            "backend":"serverW",
+            "backup":"serverZ",
             "frontedns":["fend4", "fend3"],
+            "bbdd": [
+				"bbdd3",
+				"bbdd4"
+			],
             "other": "N/A"
         }
 ]}
