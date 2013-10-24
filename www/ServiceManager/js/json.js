ca21bfcc94b70b2284e285e7d630d04ed2a10880
Julian Garcia-Sotoca <juliansotoca@gmail.com>
2013-10-24 08:25:26 +0000

    new project

---
+++ www/ServiceManager/js/json.js
@@ -0,0 +1,21 @@
+var services={"services":[
+        {
+			"service": "service1",
+            "backend":"server4",
+            "frontedns": [
+				"fend1",
+				"fend2",
+				"fend3"
+			],
+			"other": "N/A"
+        },
+        {
+			"service": "service2",
+            "backend":"server3",
+            "frontedns":["fend4", "fend3"],
+            "other": "N/A"
+        }
+]}
+//var services = '[{"service":"service1","ID":"1"}, {"service":"service2","ID":"2"}]';
+var jsonp = '[{"Language":"jQuery","ID":"1"}, {"Language":"C#","ID":"2"}]';
+var lang = '';
